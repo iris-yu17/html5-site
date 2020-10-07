70fe4394401f8bf481242eb2264be19445a079fc
Iris Yu <iris.yu0716@gmail.com>
2020-10-07 10:51:58 +0800

    add

---
+++ routes/ws-echo.js
@@ -0,0 +1,16 @@
+const WebSocket = require('ws');
+
+const createEchoServer = server => {
+    const wsServer = new WebSocket.Server({ server });
+
+    wsServer.on('connection', (ws, req) => {
+        console.log('size: ', wsServer.clients.size);
+        console.log('ip: ', req.connection.remoteAddress);
+        console.log('port: ', req.connection.remotePort);
+        ws.on('message', msg => {
+            ws.send(msg);
+        });
+        ws.send('連線了');
+    })
+}
+module.exports = createEchoServer;
